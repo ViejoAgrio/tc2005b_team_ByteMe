827f9a5d75ee78f64f6a11d22411898ed95fd306
MonicaMMartinezV <A01710965@tec.mx>
2024-04-25 11:04:53 -0600

    Merge changes Avance 3 Appix from Monica

--- Proyecto Appix/app.js
+++ Proyecto Appix/app.js
@@ -2,23 +2,31 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const loginRoutes = require('./routes/loginRoutes');
 
 // Configuración del motor de plantillas EJS
 app.set('view engine', 'ejs');
 
-// Ruta de inicio de sesión
-app.get('/login', (req, res) => {
-    res.render('login'); // Renderiza la vista login.ejs
+// Middleware para analizar el cuerpo de la solicitud
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Usar las rutas de inicio de sesión
+app.use(loginRoutes);
+
+// Ruta de nuevo-proyecto
+app.get('/nuevo-proyecto', (req, res) => {
+    res.render('nuevo-proyecto'); // Renderiza la vista nuevo-proyecto.ejs
 });
 
-// Ruta de cambio de contraseña
-app.get('/cambio-contrasena', (req, res) => {
-    res.render('cambio-contrasena'); // Renderiza la vista cambio-contrasena.ejs
+// Ruta para la página de inicio
+app.get('/', (req, res) => {
+    res.render('index'); // Renderiza la vista index.ejs
 });
 
-// Ruta para el administrador
+// Ruta para el admin
 app.get('/admin', (req, res) => {
-    res.render('admin'); // Renderiza la vista admin.ejs
+    res.render('admin'); // Renderiza la vista usuario.ejs
 });
 
 // Ruta para el usuario
@@ -26,9 +34,16 @@ app.get('/usuario', (req, res) => {
     res.render('usuario'); // Renderiza la vista usuario.ejs
 });
 
+// Configuración del directorio de archivos estáticos
+app.use(express.static('public'));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.ejs');
+});
+
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
-
