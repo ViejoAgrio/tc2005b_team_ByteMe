fa1b01e7a7a93e9511fe920d0d8eff0ca0e782f3
unknown <ViejoAgrio>
2024-04-28 21:20:11 -0600

    Reestructuración del proyecto

--- Proyecto Appix/app.js
+++ Proyecto Appix/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const loginRoutes = require('./routes/loginRoutes');
+const Routes = require('./routes/Routes');
 
 // Configuración del motor de plantillas EJS
 app.set('view engine', 'ejs');
@@ -12,35 +12,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Usar las rutas de inicio de sesión
-app.use(loginRoutes);
-
-// Ruta de nuevo-proyecto
-app.get('/nuevo-proyecto', (req, res) => {
-    res.render('nuevo-proyecto'); // Renderiza la vista nuevo-proyecto.ejs
-});
+app.use(Routes);
 
 // Ruta para la página de inicio
 app.get('/', (req, res) => {
-    res.render('index'); // Renderiza la vista index.ejs
-});
-
-// Ruta para el admin
-app.get('/admin', (req, res) => {
-    res.render('admin'); // Renderiza la vista usuario.ejs
-});
-
-// Ruta para el usuario
-app.get('/usuario', (req, res) => {
-    res.render('usuario'); // Renderiza la vista usuario.ejs
+    res.render('login'); // Renderiza la vista index.ejs
 });
 
 // Configuración del directorio de archivos estáticos
 app.use(express.static('public'));
-
-// Ruta principal
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.ejs');
-});
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
