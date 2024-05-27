d2b6acfad9883f107dfe97dda8082061526d54d7
unknown <ViejoAgrio>
2024-05-27 14:43:56 -0600

    Filters and pages bug fixed

--- Proyecto Appix/routes/admin.routes.js
+++ Proyecto Appix/routes/admin.routes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/admin.controller.js")
 const isAuth = require('../utils/is-auth');
 
 router.get('/admin', controller.render_admin);
+router.post('/admin', controller.render_admin);
 router.get('/nuevo-proyecto', controller.render_nuevo_proyecto);
 router.get('/change-password', controller.render_change_password);
 //router.post('/change-password', controller.do_change_password);
